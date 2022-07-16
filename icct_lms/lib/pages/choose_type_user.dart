dc949dce99a7ae01e6c5c3d3b20fae4e0562b964
Joe Cristian Jamis <85830010+JangManWol-source@users.noreply.github.com>
2022-07-16 17:55:20 +0800

    commit

---
+++ icct_lms/lib/pages/choose_type_user.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class ChooseUser extends StatefulWidget {
+  const ChooseUser({Key? key}) : super(key: key);
+
+  @override
+  State<ChooseUser> createState() => _ChooseUserState();
+}
+String userType = '';
+class _ChooseUserState extends State<ChooseUser> {
+
+  void goToLogin(userType){
+    Navigator.pushNamed(context, '/user_login', arguments: {
+    'user_type': userType
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Center(
+                  child: Image(
+                      image: AssetImage('assets/logo_black_text.png'), width:
+                  250,)
+                ),
+                const Center(
+                  child: Text('E-learning Management System',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 10,
+                    letterSpacing: 2
+                  ),),
+                ),
+                const SizedBox(height: 40,),
+                SizedBox(
+                  width: 150,
+                  child: ElevatedButton.icon(onPressed: (){
+                      goToLogin('Student');
+                  }, label:
+                    const Text('Student'), icon: const Icon(Icons.school),
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                SizedBox(
+                  width: 150,
+                  child: ElevatedButton.icon(onPressed: (){
+                    goToLogin('Teacher');
+                  }, label:
+                  const Text('Teacher'), icon: const Icon(Icons.person_pin_circle_rounded),
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                SizedBox(
+                  width: 150,
+                  child: ElevatedButton.icon(
+                    onPressed: (){
+                      goToLogin('Parent');
+                  }, label:
+                  const Text('Parent'), icon: const Icon(Icons.person),
+                  ),
+                )
+              ],
+            ),
+          ),
+      ),
+    );
+  }
+}
---
+++ icct_lms/lib/pages/forgot_password.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({Key? key}) : super(key: key);
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Forgot Password'),
+      ),
+    );
+  }
+}
