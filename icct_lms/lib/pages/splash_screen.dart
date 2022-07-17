93cc38d69b6e228efdd61aca56db2b8e1613819d
Joe Cristian Jamis <85830010+JangManWol-source@users.noreply.github.com>
2022-07-17 13:15:47 +0800

    SVG

--- icct_lms/lib/pages/splash_screen.dart
+++ icct_lms/lib/pages/splash_screen.dart
@@ -12,7 +12,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   void validateUser(){
      Future.delayed(const Duration(seconds: 4), (){Navigator.pushReplacementNamed(context, '/'
-         'choose_user');
+         'intro');
     });
   }
   
